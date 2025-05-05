8e30fe73bf1f1c6c332585a7dc91d83838c99236
Abhayanand Pandey <itsabhayanand@gmail.com>
2025-05-06 00:42:37 +0530

    updated

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "http://https://chat-web-1-fy85.onrender.com",
     credentials: true,
   })
 );
